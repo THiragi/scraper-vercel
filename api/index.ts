2e42d532deaec2f8b480c5471992971a130e8d8b
THiragi <w1th1nmybr41n@gmail.com>
2021-06-01 14:51:06 +0900

    add error handling

--- api/index.ts
+++ api/index.ts
@@ -4,12 +4,25 @@ import { JSDOM } from 'jsdom';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   const { query } = req;
-  if (!query.url) return res.status(404).end();
+
+  if (!query.url) {
+    return res.status(404).end();
+  }
+
+  const url = Array.isArray(query.url) ? query.url[0] : query.url;
+
+  if (!url.match(/https?:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+/g)) {
+    return res.status(404).end();
+  }
 
   try {
-    const url = Array.isArray(query.url) ? query.url[0] : query.url;
     const result = await fetch(url)
-      .then((data) => data.text())
+      .then((data) => {
+        if (!data.ok) {
+          throw new Error(`${data.status}: ${data.statusText}`);
+        }
+        return data.text();
+      })
       .then((text) => {
         const dom = new JSDOM(text);
         const meta = dom.window.document.querySelectorAll(`head > meta`);
@@ -22,14 +35,15 @@ export default async (req: VercelRequest, res: VercelResponse) => {
               .trim()
               .replace('og:', '');
             const content = ogp.getAttribute('content');
-            const obj = { [property]: content };
 
-            return { ...pre, ...obj };
+            return { ...pre, [property]: content };
           }, {});
       });
+
+    res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+
     return res.status(200).json(result);
-  } catch (e) {
-    console.log(e);
-    return res.status(500).end();
+  } catch (error) {
+    return res.status(200).json(error);
   }
 };
